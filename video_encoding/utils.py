4163bdfb119df3a2d9fd7a909ca48ef97af2d5aa
Joel Hernandez <joel@open-book.org>
2019-09-11 15:25:13 +0200

    :bug: handle non storage based files path

--- video_encoding/utils.py
+++ video_encoding/utils.py
@@ -1,20 +1,38 @@
+import os
 import tempfile
 
 
 def get_fieldfile_local_path(fieldfile):
-    storage = fieldfile.storage
-    local_temp_file = None
+    if hasattr(fieldfile, 'storage'):
+        storage = fieldfile.storage
+        local_temp_file = None
 
-    try:
-        # Try to access with path
-        storage_local_path = storage.path(fieldfile.path)
-    except (NotImplementedError, AttributeError):
-        # Storage doesnt support absolute paths, download file to a temp local dir
-        storage_file = storage.open(fieldfile.name, 'r')
-        local_temp_file = tempfile.NamedTemporaryFile(delete=False)
-        local_temp_file.write(storage_file.read())
-        local_temp_file.seek(0)
+        try:
+            # Try to access with path
+            storage_local_path = storage.path(
+                fieldfile.path
+            )
+        except (NotImplementedError, AttributeError):
+            # Storage doesnt support absolute paths, download
+            # file to a temp local dir
+            storage_file = storage.open(
+                fieldfile.name, 'r'
+            )
 
-        storage_local_path = local_temp_file.name
+            local_temp_file = tempfile.NamedTemporaryFile(
+                delete=False
+            )
+            local_temp_file.write(storage_file.read())
+            local_temp_file.seek(0)
+
+            storage_local_path = local_temp_file.name
+    else:
+        # Its a local file with no storage abstraction
+        try:
+            path = os.path.abspath(fieldfile.path)
+        except AttributeError:
+            path = os.path.abspath(fieldfile.name)
+
+        storage_local_path = path
 
     return storage_local_path, local_temp_file
