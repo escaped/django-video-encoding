5620f52bd4fdc20f38babf876349e4c6588279c7
Joel Hernandez <joel@open-book.org>
2019-09-11 12:53:31 +0200

    :construction: first attempt at using storages

--- video_encoding/files.py
+++ video_encoding/files.py
@@ -16,6 +16,7 @@ class VideoFile(File):
         Returns video width in pixels.
         """
         return self._get_video_info().get('width', 0)
+
     width = property(_get_width)
 
     def _get_height(self):
@@ -23,6 +24,7 @@ class VideoFile(File):
         Returns video height in pixels.
         """
         return self._get_video_info().get('height', 0)
+
     height = property(_get_height)
 
     def _get_duration(self):
@@ -30,6 +32,7 @@ class VideoFile(File):
         Returns duration in seconds.
         """
         return self._get_video_info().get('duration', 0)
+
     duration = property(_get_duration)
 
     def _get_video_info(self):
@@ -38,9 +41,31 @@ class VideoFile(File):
         """
         if not hasattr(self, '_info_cache'):
             encoding_backend = get_backend()
-            try:
-                path = os.path.abspath(self.path)
-            except AttributeError:
-                path = os.path.abspath(self.name)
-            self._info_cache = encoding_backend.get_media_info(path)
+
+            if hasattr(self, 'file'):
+                # Its an actual file
+                try:
+                    path = os.path.abspath(self.path)
+                except AttributeError:
+                    path = os.path.abspath(self.name)
+
+                info_cache = encoding_backend.get_media_info(path)
+            else:
+                # Its not an actual file, so assume storage abstraction
+                storage_path = getattr(self, 'path', self.name)
+                if not hasattr(self, 'storage'):
+                    raise Exception('VideoFile uses storages yet has no self.storage')
+
+                storage = self.storage
+
+                try:
+                    # If its a storage with file system implementation
+                    storage_local_path = storage.path(storage_path)
+                except NotImplementedError:
+                    storage_local_path = storage.url(storage_path)
+
+                info_cache = encoding_backend.get_media_info(storage_local_path)
+
+            self._info_cache = info_cache
+
         return self._info_cache
