056d37d18689e143a03825d893a3875448e948a4
Alexander Frenzel <alex@relatedworks.com>
2020-10-19 23:44:11 -0700

    fix: demo project was broken

--- test_proj/manage.py
+++ test_proj/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_proj.settings")
     sys.path.append(os.pardir)
 
     from django.core.management import execute_from_command_line
